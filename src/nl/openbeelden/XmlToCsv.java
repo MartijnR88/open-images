bddfdfb6bbc079ed4c5c24f4c4b2ca0aec8a5ac2
Martijn Rentmeester <martijnrentmeester@gmail.com>
2013-09-30 19:40:57 +0200

    Build new dataset tag - video

--- src/nl/openbeelden/XmlToCsv.java
+++ src/nl/openbeelden/XmlToCsv.java
@@ -22,11 +22,12 @@ import org.xml.sax.SAXException;
 
 public class XmlToCsv { 
 	public static void main(String[] args) throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {  
-		writeTagsToCsv();  
+		//writeVideoTagsToCsv();
+		writeTagVideoToCsv();
 		//writeDatasetToCsv();
 	}
 		
-	private static void writeTagsToCsv() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
+	private static void writeVideoTagsToCsv() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
 		List<List<String>> result = new ArrayList<List<String>>();
 		NodeList nodes = getNodes("tags.xml", "tags/tag");
 		
@@ -84,7 +85,68 @@ public class XmlToCsv {
 	        }    
 		}
 
-		writeToCSV(result, "tags.csv");
+		writeToCSV(result, "videotag.csv");
+	}
+	
+	private static void writeTagVideoToCsv() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
+		List<List<String>> result = new ArrayList<List<String>>();
+		NodeList nodes = getNodes("tags.xml", "tags/tag");
+		
+		//Add headers
+		List<String> headers = new ArrayList<String>();
+		headers.add("Tag");
+		headers.add("VideoName");
+		headers.add("VideoNumber");
+		headers.add("Plays");
+		headers.add("Total Videos");
+		result.add(headers);
+		
+		//Add rows
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node tag = nodes.item(i);
+	          
+	        if (tag.getNodeType() == Node.ELEMENT_NODE) {
+	            Element firstTag = (Element)tag;
+	            String tagName = firstTag.getElementsByTagName("name").item(0).getChildNodes().item(0).getNodeValue();
+	            Element mediatotal = (Element) firstTag.getElementsByTagName("media").item(0);
+	            String totalVideos = mediatotal.getAttribute("total");
+	            NodeList videos = firstTag.getElementsByTagName("video");
+	            
+	            if (videos.getLength() == 0) {
+	            	List<String> row = new ArrayList<String>();
+	            	row.add(tagName.trim());
+	            	row.add("");
+	            	row.add("");
+	            	row.add("");
+	            	row.add(totalVideos);
+	            	result.add(row);
+	            }
+	            else {
+		            for (int k = 0; k < videos.getLength(); k++){
+		            	Node video = videos.item(k);
+		            	String videoName = "";
+		    			String videoNumber = "";
+		    			String plays = "";
+		            	if (video.getNodeType() == Node.ELEMENT_NODE) {
+			                Element firstVideo = (Element)video;
+			                videoName = firstVideo.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue();
+			                videoNumber = firstVideo.getAttribute("number");
+			                plays = firstVideo.getElementsByTagName("plays").item(0).getChildNodes().item(0).getNodeValue();
+			            }
+		            	            		
+		            	List<String> row = new ArrayList<String>();
+		    			row.add(tagName.trim());
+		    			row.add(videoName.trim());
+		    			row.add(videoNumber);
+		    			row.add(plays);
+		    			row.add(totalVideos);
+		    			result.add(row);	
+		            }
+	            }
+	        }    
+		}
+
+		writeToCSV(result, "tagvideo.csv");
 	}
 	  
 	private static void writeDatasetToCsv() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException {
